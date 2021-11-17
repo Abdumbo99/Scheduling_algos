b70e0d0fb902ded2ffa6e20089170a93eaafd845
Kaan Ateşel <kaanatesel99@gmail.com>
2021-11-17 21:01:28 +0300

    file read

--- main.c
+++ main.c
@@ -5,41 +5,68 @@
 #define ARRIVAL_TIME_INDEX 1
 #define BURST_LENGTH_INDEX 2
 
-  struct ProcessesData_s {
-    int bursts_info [MAXSIZE][DATA_NUMBER];
+struct ProcessesData_s
+{
+    int bursts_info[MAXSIZE][DATA_NUMBER];
     int count;
 };
 typedef struct ProcessesData_s ProcessesData;
 
-void add_process_data (ProcessesData* processData, int arrivalTime, int burstLength){
+void add_process_data(ProcessesData *processData, int arrivalTime, int burstLength)
+{
     processData->bursts_info[processData->count][BURST_NUMBER_INDEX] = processData->count;
     processData->bursts_info[processData->count][ARRIVAL_TIME_INDEX] = arrivalTime;
     processData->bursts_info[processData->count][BURST_LENGTH_INDEX] = burstLength;
     processData->count++;
 }
 // in use , left time, and we have to increment counter for all in use processes
-double turnaround_time_FCFS (int processDataArray [MAXSIZE][DATA_NUMBER], int count){
+double turnaround_time_FCFS(int processDataArray[MAXSIZE][DATA_NUMBER], int count)
+{
     return 0.0;
 }
-double turnaround_time_SJF (int processDataArray [MAXSIZE][DATA_NUMBER], int count){
+double turnaround_time_SJF(int processDataArray[MAXSIZE][DATA_NUMBER], int count)
+{
     return 0.0;
 }
-double turnaround_time_SRTF (int processDataArray [MAXSIZE][DATA_NUMBER], int count){
+double turnaround_time_SRTF(int processDataArray[MAXSIZE][DATA_NUMBER], int count)
+{
     return 0.0;
 }
-double turnaround_time_RR (int processDataArray [MAXSIZE][DATA_NUMBER], int count, int q){
+double turnaround_time_RR(int processDataArray[MAXSIZE][DATA_NUMBER], int count, int q)
+{
     return 0.0;
 }
 
-int main() {
+int main()
+{
     ProcessesData prData;
     prData.count = 0;
 
-    add_process_data (&prData, 15, 175);
-    add_process_data (&prData, 20, 200);
+    add_process_data(&prData, 15, 175);
+    add_process_data(&prData, 20, 200);
 
-    printf("count is %d and arrival time %d and length is %d \n", prData.count, prData.bursts_info[0][1], prData.bursts_info[0][2]);
-    printf("count is %d and arrival time %d and length is %d \n", prData.count, prData.bursts_info[1][1], prData.bursts_info[1][2]);
+    FILE *fp;
+    char *name = "input.txt";
+    printf("file name %s \n", name);
+
+    fp = fopen(name, "r");
+    if (fp != NULL)
+    {
+        int index;
+        int arrivalTime;
+        int burstTime;
+        while (fscanf(fp, "%d %d %d", &index, &arrivalTime, &burstTime) != EOF)
+        {
+            printf("%d %d %d\n", index, arrivalTime, burstTime);
+            add_process_data(&prData, arrivalTime, burstTime);
+            
+        }
+    }
+
+    for (int i = 0; i < prData.count; i++)
+    {
+        printf("count is %d and arrival time %d and length is %d \n", prData.count, prData.bursts_info[i][1], prData.bursts_info[i][2]);
+    }
 
     return 0;
 }
